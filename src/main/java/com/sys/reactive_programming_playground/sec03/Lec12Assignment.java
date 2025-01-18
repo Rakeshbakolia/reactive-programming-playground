44135f519ce6569a81f51d72bd3d938c1de81c45
Rakesh Bakolia <rakeshbakolia95@gmail.com>
2025-01-18 16:59:04 +0530

    sec03

---
+++ src/main/java/com/sys/reactive_programming_playground/sec03/Lec12Assignment.java
@@ -0,0 +1,18 @@
+package com.sys.reactive_programming_playground.sec03;
+
+import com.sys.reactive_programming_playground.common.Util;
+import com.sys.reactive_programming_playground.sec03.assignment.StockPriceObserver;
+import com.sys.reactive_programming_playground.sec03.client.ExternalServiceClient;
+
+public class Lec12Assignment {
+
+    public static void main(String[] args) {
+
+        var client = new ExternalServiceClient();
+        var subscriber = new StockPriceObserver();
+        client.getPriceChanges()
+                .subscribe(subscriber);
+
+        Util.sleepSecond(20);
+    }
+}
