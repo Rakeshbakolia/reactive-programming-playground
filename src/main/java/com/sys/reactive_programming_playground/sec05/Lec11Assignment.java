99f612f64c6d4cc0e2d586e1812acc11597cd585
Rakesh Bakolia <rakeshbakolia95@gmail.com>
2025-01-20 02:04:52 +0530

    sec05 done

---
+++ src/main/java/com/sys/reactive_programming_playground/sec05/Lec11Assignment.java
@@ -0,0 +1,18 @@
+package com.sys.reactive_programming_playground.sec05;
+
+import com.sys.reactive_programming_playground.common.Util;
+import com.sys.reactive_programming_playground.sec05.assignmnet.ExternalServiceClient;
+
+public class Lec11Assignment {
+
+    public static void main(String[] args) {
+        var client = new ExternalServiceClient();
+
+        for (int i = 0; i <5;i++){
+            client.getProductName(i)
+                    .subscribe(Util.subscriber());
+        }
+
+        Util.sleepSecond(3);
+    }
+}
